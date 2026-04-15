8a6673ff2339a15432644146bb759e1d823626d8
이영욱 <pr@lyuk98.com>
2026-04-15 21:35:06 +0900

    Remove Graphviz from development environment
    I do not think tofu graph will be necessary.

--- default.nix
+++ default.nix
@@ -6,7 +6,6 @@
   # The default development environment
   default = pkgs.mkShellNoCC {
     nativeBuildInputs = with pkgs; [
-      graphviz # for converting DOT files
       opentofu
       python3
     ];
